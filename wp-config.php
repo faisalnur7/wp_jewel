5cb2bd68a4f66d814b0ef5fec8fdbfbaa9819d86
faisalnur7 <md.faisalnur55@gmail.com>
2026-08-01 01:07:15 +0600

    Enable local debug config

--- wp-config.php
+++ wp-config.php
@@ -113,7 +113,7 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
 define( 'WP_DEBUG_DISPLAY', true );
 @ini_set( 'display_errors', '1' );
